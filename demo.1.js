96befe95ef1cc3857ddbe818c0a16d9e0d383468
priyanka2798 <ankapriya027@gmmail.com>
2023-05-27 07:40:07 +0530

    final commit

--- demo.1.js
+++ demo.1.js
@@ -15,28 +15,35 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var description=document.getElementById('description').value
 
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
+
+  //const newText=document.createTextNode(inputValue)
+  var description=document.createTextNode(" "+description)
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
-
+  li.appendChild(description)
   // Create del button element
   var deleteBtn = document.createElement('button');
-
+  var editBtn=document.createElement('button')
   // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
+  editBtn.className='btn-sm float-right edit'
   // Append text node
   deleteBtn.appendChild(document.createTextNode('X'));
+  editBtn.appendChild(document.createTextNode('Edit'))
 
   // Append button to li
   li.appendChild(deleteBtn);
+  li.appendChild(editBtn)
 
   // Append li to list
   itemList.appendChild(li);
+  itemList.appendChild(li)
 }
 
 // Remove item
@@ -58,7 +65,8 @@ function filterItems(e){
   // Convert to an array
   Array.from(items).forEach(function(item){
     var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
+    var description=item.childNodes[1].textContent
+    if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1){
       item.style.display = 'block';
     } else {
       item.style.display = 'none';
